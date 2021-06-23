b51a25a5476b9d7fd8e2865829b3077e12de1873
Jimmy Mooney <jimmymooney@jimmyhcacbook16.lan>
2021-06-23 17:38:02 -0400

    improved runtime by 12% by utilizing a hashtable to store votes so each successive round of calculation doesn't need to check every vote, just the ones who voted for the loser

--- ranked_choice.py
+++ ranked_choice.py
@@ -3,7 +3,7 @@ import random, pprint
 NUM_VOTERS = 1000000
 MAX_CHOICES = 5
 
-cVotes = { "A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0, "G": 0, "H": 0 }
+cVotes = { "A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": [] }
 
 vStats = { 	"A": {"0": 0,"1": 0,"2": 0,"3": 0,"4": 0}, 
 			"B": {"0": 0,"1": 0,"2": 0,"3": 0,"4": 0}, 
@@ -19,8 +19,17 @@ vChoiceStats = {"1": 0,"2": 0,"3": 0,"4": 0, "5": 0}
 
 votes = []
 
+def printRoundTotals(r):
+	print "Round " + str(r) + " totals:\n"
+
+	for x in cVotes.keys():
+		print x + ": " + str(len(cVotes[x]));
+
+	print "\n"
+
+
 def goVote():
-    while len(votes) < NUM_VOTERS:
+    for v in range(0,NUM_VOTERS):
     	thisVote = []
     	cList = cVotes.keys()
     	numVotes = random.randrange(MAX_CHOICES)+1
@@ -31,43 +40,39 @@ def goVote():
     		vStats[cList[randVote]][str(x)] += 1
     		del cList[randVote]
 
-    	votes.append(thisVote);
-
-    	if thisVote[0] not in cVotes.keys():
-    		cVotes[thisVote[0]] = 1
-    	else:
-    		cVotes[thisVote[0]] += 1
+    	cVotes[thisVote[0]].append(thisVote);
 
 def tabulateVotes(roundNum=1):
 	lowestVoteCount = cVotes.keys()[0]
 	totalVotes = 0
 	for x in cVotes.keys():
-		totalVotes += cVotes[x]
+		totalVotes += len(cVotes[x])
 	for x in cVotes.keys():
-		if float(cVotes[x]) / float(totalVotes) <= 0.500:
-			if cVotes[x] < cVotes[lowestVoteCount]:
+		if float(len(cVotes[x])) / float(totalVotes) <= 0.500:
+			if len(cVotes[x]) < len(cVotes[lowestVoteCount]):
 				lowestVoteCount = x
 		else:
 			return x + " WINS!"
 
-	for v in range(0,len(votes)):
-		if len(votes[v]) > 0:
-			if votes[v][0] == lowestVoteCount:
-				del votes[v][0]
-				while len(votes[v]) > 0 and votes[v][0] not in cVotes.keys():
-					del votes[v][0]
-				if len(votes[v]) > 0:
-					cVotes[votes[v][0]] += 1
+	for v in range(0,len(cVotes[lowestVoteCount])):
+		del cVotes[lowestVoteCount][v][0]
+
+		while len(cVotes[lowestVoteCount][v]) > 0 and cVotes[lowestVoteCount][v][0] not in cVotes.keys():
+			del cVotes[lowestVoteCount][v][0]
+
+		if len(cVotes[lowestVoteCount][v]) > 0:
+			cVotes[cVotes[lowestVoteCount][v][0]].append(cVotes[lowestVoteCount][v])
+
 	cVotes.pop(lowestVoteCount, None)
-	print "Round " + str(roundNum+1) + " totals: \n" + str(cVotes)
+	printRoundTotals(roundNum)
 
 	return tabulateVotes(roundNum+1)
 
 goVote()
 
-print "Round 1 totals:\n" + str(cVotes)
+printRoundTotals(1)
 
-print tabulateVotes()
+print tabulateVotes(2)
 
 print "How Candidates Ranked"
 pprint.pprint(vStats)
